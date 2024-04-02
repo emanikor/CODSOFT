96a56f989deea88a337ca6679296fd27f2e9fbfc
Emanikor <87859876+emanikor@users.noreply.github.com>
2024-04-02 11:33:32 +0300

    Add files via upload

---
+++ Simple-calculator/index.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+       switch(e.target.innerText){
+        case 'c':
+            display.innerText = '';
+            break;
+            case '←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0,-1);
+                }
+                break;
+                case '=':
+                    try{
+                        display.innerText =eval(display.innerText); 
+                        break;
+                    }
+                
+                    catch{
+                        display.innerText= 'syntax error!';
+                        {
+                        display.innerText.buttons='';
+                        }
+                    }
+                   
+        default: 
+           display.innerText+=e.target.innerText;
+       
+       }
+    })
+});
